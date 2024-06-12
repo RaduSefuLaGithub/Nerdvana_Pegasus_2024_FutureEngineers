c6884a7dab790f9522e8704907a7884dcdc75ae1
Radu Catana <rrradufirst@gmail.com>
2024-06-12 22:30:37 +0300

    New Code

--- Nerdvana_Pegasus_2024-main/src/final/codfe/codfe.ino
+++ Nerdvana_Pegasus_2024-main/src/final/codfe/codfe.ino
@@ -1,18 +1,140 @@
 #include <ESP32Servo.h>
 #include <BMI088.h>
+#include <NewPing.h>
 
 Servo servo;  // create servo object to control a servo
 
+// Gyro sensor
+#define GYRO_SAMPLE_SIZE 1
+#define DRIFT_TEST_TIME 10
+#define INT_PIN A6
+
+// Gyro sensor
 Bmi088Gyro gyro(Wire,0x69);
+Bmi088Accel accel(Wire,0x19);
+
+// Gyro sensor
+double gyro_last_read_time = 0;
+double drifts_x = 0, drifts_y = 0, drifts_z = 0;
+bool gyro_flag, accel_flag;
+double gx, gy, gz;
+long cnt1 = 0;
+
+/// Gyro functions
+
+void gyro_drdy()
+{
+  gyro_flag = true;
+}
+
+void accel_drdy()
+{
+  accel_flag = true;
+}
+
+void gyro_setup(bool debug) {
+  int status = accel.begin();
+  status = accel.setOdr(Bmi088Accel::ODR_200HZ_BW_80HZ);
+  status = accel.pinModeInt1(Bmi088Accel::PUSH_PULL,Bmi088Accel::ACTIVE_HIGH);
+  status = accel.mapDrdyInt1(true);
+
+
+  status = gyro.begin();
+
+  status = gyro.setOdr(Bmi088Gyro::ODR_400HZ_BW_47HZ);
+  status = gyro.pinModeInt3(Bmi088Gyro::PUSH_PULL,Bmi088Gyro::ACTIVE_HIGH);
+  status = gyro.mapDrdyInt3(true);
+
+  pinMode(INT_PIN,INPUT);
+  attachInterrupt(INT_PIN,gyro_drdy,RISING);  
+
+
+  if(status < 0) {
+    if(debug) {
+      Serial.print("BMI Initialization Error!  error: ");
+      Serial.println(status);
+    }
+  }
+  else  {
+    // Gyro drift calculation
+    if(debug) Serial.println("Starting gyro drift calculation...");
+
+    gx = 0;
+    gy = 0;
+    gz = 0;
+
+    gyro_last_read_time = millis();
+
+    double start_time = millis();
+    while(millis() - start_time < DRIFT_TEST_TIME * 1000) {
+      gyro.readSensor();  
+      double read_time = millis();
+      gx += (gyro.getGyroX_rads() * (read_time - gyro_last_read_time) * 0.001);
+      gy += (gyro.getGyroY_rads() * (read_time - gyro_last_read_time) * 0.001);
+      gz += (gyro.getGyroZ_rads() * (read_time - gyro_last_read_time) * 0.001);
+
+      gyro_last_read_time = read_time;
+    }
+
+    drifts_x = gx / DRIFT_TEST_TIME;
+    drifts_y = gy / DRIFT_TEST_TIME;
+    drifts_z = gz / DRIFT_TEST_TIME;
+
+    if(debug) Serial.print("Drift test done!\nx: ");
+    if(debug) Serial.print(drifts_x, 6);
+    if(debug) Serial.print("   y: ");
+    if(debug) Serial.print(drifts_y, 6);
+    if(debug) Serial.print("   z: ");
+    if(debug) Serial.println(drifts_z, 6);
+  }
+  // Gyro value reset
+  gx = 0;
+  gy = 0;
+  gz = 0;
+
+  gyro_last_read_time = millis();
+}
+
+void read_gyro(bool debug) {
+  //delta_start = millis();
+  if(gyro_flag) {
+    gyro_flag = false;
+    cnt1++;
+    gyro.readSensor();   
+    double read_time = millis();
+
+    gx += ((gyro.getGyroX_rads() - drifts_x) * (read_time - gyro_last_read_time) * 0.001) * 180.0 / PI;
+    gy += ((gyro.getGyroY_rads() - drifts_y) * (read_time - gyro_last_read_time) * 0.001) * 180.0 / PI;
+    gz += ((gyro.getGyroZ_rads() - drifts_z) * (read_time - gyro_last_read_time) * 0.001) * 180.0 / PI;
+
+    gyro_last_read_time = read_time;
+
+    //delta_gyro = millis() - delta_start;
+
+    if(debug) {
+      Serial.print("Gyro: gx: ");
+      Serial.print(gx);
+      Serial.print(" gy: ");
+      Serial.print(gy);
+      Serial.print(" gz: ");
+      Serial.println(gz);
+    }
+  }
+}
 
 const int trigFront = 2;
 const int echoFront = 3;
-////const int trigSide = 4;
-const int echoSide = 5;
+const int trigDR = 10;
+const int echoDR = 11;
+const int MaxSensorDist = 150;
+const int BUTTON_PIN = 12;
 const int PWMA = 6;
 const int AIN1 = 7;
 const int AIN2 = 8;
 const int servoPin = 9;
+
+NewPing sonarFR(trigFront, echoFront, MaxSensorDist);
+NewPing sonarDR(trigDR, echoDR, MaxSensorDist);
 
 // LEDC PWM channels and frequency
 const int freq = 5000;
@@ -20,18 +142,23 @@ const int pwmChannel = 1;
 const int resolution = 10;
 
 long duration;
-int distanceFront, distanceSide;
+int distanceFront, distanceST, distanceDR;
 
+int HighServoLimit;
+int LowServoLimit;
+int MiddleServoDeg;
 void setup() {
   pinMode(trigFront, OUTPUT);
   pinMode(echoFront, INPUT);
-  //pinMode(trigSide, OUTPUT);
-  //pinMode(echoSide, INPUT);
+  pinMode(trigDR, OUTPUT);
+  pinMode(echoDR, INPUT);
   pinMode(AIN1, OUTPUT);
   pinMode(AIN2, OUTPUT);
 
-  Serial.begin(9600);
-  while (!Serial) ;
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(115200);
+  //while (!Serial) ;
 
   // Enable the motor driver
   digitalWrite(AIN1, LOW);
@@ -43,64 +170,191 @@ void setup() {
   ledcWrite(pwmChannel, 0);
 
   servo.attach(servoPin, 544, 1850);  // attaches the servo on pin 9 to the servo object
-  servo.write(90);
+  delay(1000);
+  servo.write(110);
+  delay(1000);
+  //Serial.println(servo.read());
+  HighServoLimit = servo.read();
+  servo.write(70);
+  delay(1000);
+  //Serial.println(servo.read());
+  LowServoLimit = servo.read();
 
-  /*int status;
-  status = gyro.begin();
-  if(status>=0)
-    Serial.println("Gyro Is set!");
-  else if(status<0)
-    Serial.println("Gyro Failled Setup!");*/
+  //MiddleServoDeg = (HighServoLimit+LowServoLimit)/2;
+  MiddleServoDeg = 87;
+  //Serial.println(MiddleServoDeg);
+  servo.write(MiddleServoDeg);
+  delay(2000);
+  gyro_setup(true);
+  digitalWrite(LED_BUILTIN, HIGH);
+  waitForButtonPress();
+}
 
-  //Serial.println("1");
-  //Serial.println(status);
+void waitForButtonPress() {
+  pinMode(BUTTON_PIN, INPUT_PULLUP);  // Configure button pin as input with internal pull-up resistor
 
-  //gyro.mapDrdyInt3(true);
+  Serial.println("Waiting for button press...");
+  while (digitalRead(BUTTON_PIN) == HIGH) {
+    // Wait here until the button is pressed
+  }
+  Serial.println("Button pressed, starting loop...");
 }
 
 
-float gx, gy, gz;
 int wasmotorstopped = 0;
+int error = 0;
+int degServo = 0;
+int currMiddleZ = 0;
+int distminFront = 45;
+int turns = 0;
+int maxstreak = 1;
+int streak = 0;
+int turnDir = 0;
+int timer;
+int timerlenght;
 
-void loop() {
-  //Serial.println("2");
-  distanceFront = getDistance(trigFront, echoFront);
-  Serial.print("DistanceF: ");
-  Serial.println(distanceFront);
-  //distanceSide = getDistance(trigSide, echoSide);
-  //Serial.print("DistanceS: ");
-  //Serial.println(distanceSide);
-
-  if (distanceFront < 50) // if an obstacle is detected in front
+void loop()
+{
+  distanceFront = distminFront+10;
+  //distanceDR = getDistance(trigDR, echoDR);
+  distanceDR = sonarDR.ping_cm();
+  //delay(100);
+  while(distanceFront > distminFront || distanceFront<=10)
   {
-      stopMotor();             // stop motor
+    read_gyro(false);
+    //delay(100);
+    if(gz>=currMiddleZ)
+    {
+      error = gz-currMiddleZ;
+      degServo = MiddleServoDeg-error;
+    }
+    else if(gz<currMiddleZ)
+    {
+      error = currMiddleZ-gz;
+      degServo = MiddleServoDeg+error;
+    }
+    Serial.print(degServo);
+    Serial.print("  ");
+    Serial.print(distanceDR);
+    Serial.print("  ");
+    Serial.println(distanceFront);
+    if(degServo>HighServoLimit)
+      degServo=HighServoLimit;
+    if(degServo<LowServoLimit)
+      degServo=LowServoLimit;
+    servo.write(degServo);
+    moveForward(500);
+
+    if(currMiddleZ-5<=gz && gz<=currMiddleZ+5)
+    {
+      streak++;
+      if(streak==maxstreak)
+      {
+        //distanceFront = getDistance(trigFront, echoFront);
+        distanceFront = sonarFR.ping_cm();
+        //delay(100);
+        streak = 0;
+      }
+      else
+        distanceFront = distminFront+10;
+    }
+    else
+      streak = 0;
+    //distanceDR = getDistance(trigDR, echoDR);
+    //distanceDR = sonarDR.ping_cm();
+    //delay(100);
   }
-  else
+  stopMotor();
+
+  if(turns!=12)
   {
-    moveForward(1023);
+    Serial.print("Turning... ");
+    Serial.println(distanceFront);
+    distanceDR = sonarDR.ping_cm();
+    //delay(1000);
+    Serial.println(distanceDR);
+    //delay(1000);
+    if(turns==0)
+    {
+      if(distanceDR!=0 && distanceDR<=90)
+        turnDir = 1;
+        Serial.print("DistDR: ");
+        Serial.println(distanceDR);
+    }
+    if(turnDir==0)
+    {
+      currMiddleZ += 90;
+      degServo = HighServoLimit;
+    }
+    else if(turnDir==1)
+    {
+      currMiddleZ -= 90;
+      degServo = LowServoLimit;
+    }
+    servo.write(degServo);
+    //delay(1000);
+    read_gyro(true);
+    //delay(100);
+    if(turnDir==0)
+    {
+      while(gz+2<currMiddleZ)
+      {
+        moveForward(400);
+        read_gyro(true);
+       // delay(100);
+      }
+    }
+    else if(turnDir==1)
+    {
+      while(gz-2>currMiddleZ)
+      {
+        moveForward(400);
+        read_gyro(true);
+        //delay(100);
+      }
+    }
+    stopMotor();
+    Serial.println("Turn Complete!");
+    degServo = MiddleServoDeg;
+    servo.write(degServo);
+    turns++;
+    if(turns==12)
+    {
+      distminFront = 160;
+      timer = millis();
+      while(millis()-timer<=2000)
+      {
+        read_gyro(false);
+        //delay(100);
+        if(gz>=currMiddleZ)
+        {
+          error = gz-currMiddleZ;
+          degServo = MiddleServoDeg-error;
+        }
+        else if(gz<currMiddleZ)
+        {
+          error = currMiddleZ-gz;
+          degServo = MiddleServoDeg+error;
+        }
+        if(degServo>HighServoLimit)
+          degServo=HighServoLimit;
+        if(degServo<LowServoLimit)
+          degServo=LowServoLimit;
+        servo.write(degServo);
+        moveForward(500);
+      }
+      stopMotor();
+    }
+    //delay(1000);
   }
-    /*int val;
-    for(val=0; val<25; val++)
-      servo.write(val);
-    for(val=25; val>0; val--)
-      servo.write(val);*/
+  if(turns==12)
+  {
+    while(true)
+    {
 
-      /*gyro.readSensor();
-
-      gx = gyro.getGyroX_rads();
-      gy = gyro.getGyroY_rads();
-      gz = gyro.getGyroZ_rads();
-
-      Serial.print("gx:");
-      Serial.print(gx);
-
-      Serial.print(", gy:");
-      Serial.print(gy);
-
-      Serial.print(", gz:");
-      Serial.println(gz);
-      delay(1000);*/
-      digitalWrite(13, HIGH);
+    }
+  }
+  //read_gyro(true);
 }
 
 int getDistance(int trigPin, int echoPin) {
